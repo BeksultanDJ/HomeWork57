5b151dd9c3a2eb576f96212fc446eb641033abe2
Beksultan Dzhienbekov <bekadj14@gmail.com>
2023-11-08 01:29:49 +0600

    сделал запрос и вывод имени, почты и роли

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,59 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 
 function App() {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [role, setRole] = useState('');
+    const [userData, setUserData] = useState(null);
 
-  return (
-    <>
+    const addUser = () => {
+        const newUser = {
+            name: name,
+            email: email,
+            role: role,
 
-    </>
-  )
+        };
+
+        setUserData(newUser);
+    };
+
+    return (
+        <>
+            <div className="UserForm">
+                <input
+                    placeholder="Имя"
+                    name='name'
+                    type="text"
+                    value={name}
+                    onChange={(event) => setName(event.target.value)}
+                />
+                <input
+                    placeholder="Email"
+                    name='email'
+                    type="email"
+                    value={email}
+                    onChange={(event) => setEmail(event.target.value)}
+                />
+                <select name="role" onChange={(event) => setRole(event.target.value)}>
+                    <option id="role" value="User">Пользователь</option>
+                    <option id="role" value="Editor">Редактор</option>
+                    <option id="role" value="Admin">Администратор</option>
+                </select>
+                <button onClick={addUser}>Submit</button>
+            </div>
+
+            <div className="UserItem">
+                {userData && (
+                    <div>
+                        <p>Name: {userData.name}</p>
+                        <p>Email: {userData.email}</p>
+                        <p>Role: {userData.role}</p>
+                    </div>
+                )}
+            </div>
+        </>
+    );
 }
 
-export default App
+export default App;
